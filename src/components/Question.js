4cda3aa606b23714389cabc5cd8b8ad8f41ee01d
Zamzam95-Coder <zam19@rogers.com>
2021-01-16 21:26:02 -0500

    test

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,6 @@
 
 import React, {useEffect, useState} from 'react';
+import '../App.css';
 
 function Question(props) {
   const { questionText, valueChange } = props;
@@ -12,17 +13,17 @@ function Question(props) {
 
   return (
     <div className = "main">
-      <form>
-        <h1> {props.question} </h1>
-        <label for="s">{props.option1}</label>
-        <input type="radio" name={props.name} id="option1" value={props.value1} onChange={e => onValueChange(e)}/>
-        <label for="m">{props.option2}</label>
-        <input type="radio" name={props.name} id="option2" value={props.value2} onChange = {e => onValueChange(e)}/>
-        <label for="l">{props.option3}</label>
-        <input type="radio" name={props.name} id="option3" value={props.value3} onChange = {e => onValueChange(e)}/>
-        <label for="l">{props.option4}</label>
-        <input type="radio" name={props.name} id="option4" value={props.value4} onChange = {e => onValueChange(e)}/>
-      </form>
+      <h1> {props.question} </h1>
+        <div className = "radio-buttons-all">
+          <input type="radio" name={props.name} id="option1" value={props.value1} onChange={e => onValueChange(e)}/>
+          <label for="option1">{props.option1}</label>
+          <input type="radio" name={props.name} id="option2" value={props.value2} onChange = {e => onValueChange(e)}/>
+          <label for="option2">{props.option2}</label>
+          <input type="radio" name={props.name} id="option3" value={props.value3} onChange = {e => onValueChange(e)}/>
+          <label for="option3">{props.option3}</label>
+          <input type="radio" name={props.name} id="option4" value={props.value4} onChange = {e => onValueChange(e)}/>
+          <label for="option4">{props.option4}</label>
+        </div>
     </div>
   );
 }
