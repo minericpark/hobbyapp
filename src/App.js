97ee58b433473df5a4d478f8eacf00d69cabad78
detols <mpsericpark@gmail.com>
2021-01-17 01:04:05 -0500

    Added document title and ico

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import * as Firestore from './services/Firestore';
-import Question from './components/Question';
 import React, {useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import QuestionsPage from './QuestionsPage.js';
